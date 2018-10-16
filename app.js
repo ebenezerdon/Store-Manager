da29e354ec58c4d83e9505678e0cb8bbd06f7c19
Ebenezer <ebenezerdonu@gmail.com>
2018-10-16 07:31:47 +0100

    [ch #161242889] Add db to models folder

---
+++ app.js
@@ -0,0 +1,36 @@
+import createError from 'http-errors';
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './server/routes/index';
+import usersRouter from './server/routes/users';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'UI')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
---
+++ server/bin/models/products.js
@@ -0,0 +1,42 @@
+
+const products = [
+  {
+    id: 1,
+    name: 'Long Sleeve Hooded T Shirt- Wine & Grey',
+    description: 'Long Sleeve Hooded T Shirt- Wine & Grey',
+    prize: '₦3,300',
+    quantity: '50',
+    min: '10',
+    max: '100'
+  },
+  {
+    id: 2,
+    name: 'Fashion Quality Ankara Fabrics For Women-multi-colour',
+    description: 'Fashion Quality Ankara Fabrics For Women-multi-colour',
+    prize: '₦7,108',
+    quantity: '40',
+    min: '5',
+    max: '70'
+  },
+  {
+    id: 3,
+    name: 'Breathable Sneakers For Men',
+    description: 'Breathable Sneakers For Men',
+    prize: '₦4,995',
+    quantity: '33',
+    min: '5',
+    max: '70'
+  },
+  {
+    id: 4,
+    name: 'Luxe Linient Solution Towels',
+    description: 'Luxe Linient Solution Towels',
+    prize: '₦13,550',
+    quantity: '20',
+    min: '10',
+    max: '100'
+  },
+  
+];
+
+export default products;
