719114e42f1b46c5f170be883516212491e9e019
Ebenezer <ebenezerdonu@gmail.com>
2018-10-19 13:24:19 +0100

    bg[161341637] add --exit flag to test script

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './server/routes/index';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
 const app = express();
 
 app.set('port', process.env.PORT || 5000);
+app.set('json spaces', 2);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // use body parser to parse request
 app.use(bodyParser.urlencoded({ extended: true }));
