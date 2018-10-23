5cff4b6488abfe24ef9e1901dfe051c93cbbfc1d
Ebenezer <ebenezerdonu@gmail.com>
2018-10-23 10:24:52 +0100

    fix(verify): restrict unauthorized users

---
+++ server/controllers/ProductsController.js
@@ -0,0 +1,50 @@
+import productsModel from '../models/productsModel';
+
+class ProductsController {
+  /* Gets all products */
+  static getAll(req, res) {
+    return (
+      res.status(200).json(products)
+    );
+  }
+
+  /* Gets one from products list */
+  static getOne(req, res) {
+    if ((req.params.id) > products.length) {
+      return (
+        res.status(404).json('Hi! Can you check again? There\'s no product with that id')
+      );
+    } 
+    return (
+      res.status(200).json(products[req.params.id - 1])
+    );
+  }
+
+  /* Adds a product */
+  static addProduct(req, res) {
+    const product = new Products(); // creates a new instance of Products model
+    product.id = products.length + 1;
+    product.name = req.body.name;
+    product.description = req.body.description;
+    product.price = req.body.price;
+    product.quantity = req.body.quantity;
+
+    products.push(product);
+
+    if (!req.body.name && !req.body.price) {
+      return (
+        res.status(400).json('Hi! Can you try again? Your input is invalid')
+      );
+    } else {
+      return (
+        res.status(201).json({
+          message: 'Product added!',
+          product
+        })
+      );
+    }
+  }
+
+}
+
+export default ProductsController;
