5a1a97724164b7eee2932fd093ce9d3bc8f0c260
Ebenezer <ebenezerdonu@gmail.com>
2018-10-24 22:40:11 +0100

    bg-change-classes-to-es6-functions-161463196

--- server/controllers/ProductsController.js
+++ server/controllers/ProductsController.js
@@ -1,50 +1,53 @@
-import productsModel from '../models/productsModel';
+import products from '../models/products';
 
-class ProductsController {
-  /* Gets all products */
-  static getAll(req, res) {
+/* Gets all products */
+const getAllProducts = (req, res) => {
+  return (
+    res.status(200).json(products)
+  );
+};
+
+/* Gets one from products list */
+const getOneProduct = (req, res) => {
+  if ((req.params.id) > products.length) {
     return (
-      res.status(200).json(products)
+      res.status(404).json('Hi! Can you check again? There\'s no product with that id')
     );
   }
+  return (
+    res.status(200).json(products[req.params.id - 1])
+  );
 
-  /* Gets one from products list */
-  static getOne(req, res) {
-    if ((req.params.id) > products.length) {
-      return (
-        res.status(404).json('Hi! Can you check again? There\'s no product with that id')
-      );
-    } 
+};
+
+/* Adds a product */
+const addProduct = (req, res) => {
+  const product = {
+    id: products.length + 1,
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    quantity: req.body.quantity,
+  };
+
+  products.push(product);
+
+  if (!req.body.name && !req.body.price) {
     return (
-      res.status(200).json(products[req.params.id - 1])
+      res.status(400).json('Hi! Can you try again? Your input is invalid')
     );
   }
+  return (
+    res.status(201).json({
+      message: 'Product added!',
+      product,
+    })
+  );
 
-  /* Adds a product */
-  static addProduct(req, res) {
-    const product = new Products(); // creates a new instance of Products model
-    product.id = products.length + 1;
-    product.name = req.body.name;
-    product.description = req.body.description;
-    product.price = req.body.price;
-    product.quantity = req.body.quantity;
+};
 
-    products.push(product);
-
-    if (!req.body.name && !req.body.price) {
-      return (
-        res.status(400).json('Hi! Can you try again? Your input is invalid')
-      );
-    } else {
-      return (
-        res.status(201).json({
-          message: 'Product added!',
-          product
-        })
-      );
-    }
-  }
-
-}
-
-export default ProductsController;
+export {
+  getAllProducts,
+  getOneProduct,
+  addProduct,
+};
---
+++ server/controllers/usersController.js
@@ -0,0 +1,82 @@
+import jwt from 'jsonwebtoken';
+import users from '../models/usersModel';
+import config from '../configuration/config';
+
+const { secret } = config;
+
+const getAllUsers = (req, res) => {
+  return (
+    res.status(200).json(users)
+  );
+};
+
+const getOneUser = (req, res) => {
+  if ((req.params.id) > users.length) {
+    return (
+      res.status(404).json('Hi! Can you check again? There\'s no user with that id')
+    );
+  }
+
+  return (
+    res.status(200).json(users[req.params.id - 1])
+  );
+};
+
+/* Adds a new user */
+const addUser = (req, res) => {
+  const user = {
+    id: users.length + 1,
+    fullName: req.body.fullName,
+    emailAdress: req.body.emailAdress,
+    password: req.body.password,
+    type: req.body.type,
+    createdAt: new Date(),
+  };
+  users.push(user);
+
+  if (!req.body.fullName && !req.body.emailAdress &&
+    !req.body.password) {
+    return (
+      res.status(400).json('Hi! Can you try again? Your input is invalid')
+    );
+  }
+  return (
+    res.status(201).json({
+      message: 'New user added!',
+      user,
+    })
+  );
+};
+
+const loginUser = (req, res) => {
+  const {
+    emailAdress,
+    password,
+    type,
+  } = req.body;
+  let authDetail;
+  let userFound = false;
+  users.map((user) => {
+    if (emailAdress === user.emailAdress &&
+      password === user.password && type === user.type) {
+      userFound = true;
+      authDetail = user;
+    }
+  });
+  if (userFound) {
+    const token = jwt.sign(authDetail, secret, {
+      expiresIn: '24hr',
+    });
+    return (
+      res.status(200).json({ token })
+    );
+  }
+  return (
+    res.status(404).json({
+      message: 'Hi! Can you check again? Ther\'s no user with that id',
+    }));
+};
+
+export {
+  getAllUsers, getOneUser, addUser, loginUser,
+};
