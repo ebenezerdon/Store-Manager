64eb55e38fe76c579ad05601b90e05e80adc7d09
Ebenezer <ebenezerdonu@gmail.com>
2018-10-25 11:10:56 +0100

    ch-remove-repeated-code-from-verify-161465898

--- server/middleware/verify.js
+++ server/middleware/verify.js
@@ -10,41 +10,44 @@ const authenticate = (req, res, next) => {
       message: 'Hi! You\'re not authorized to use this resource.',
     });
   }
-  jwt.verify(token, secret, (error) => {
+  jwt.verify(token, secret, (error, decoded) => {
+      console.log(decoded);
     if (error) {
       return res.status(401).json({
         message: 'Invalid authencation! Can you check and try again?',
         error: true,
       });
     }
+    req.decoded = decoded;
     return next();
   });
 };
 
+
 const verifyAdmin = (req, res, next) => {
-  const token = req.headers.accesstoken;
-  jwt.verify(token, secret, (err, authdata) => {
-    if (authdata.type !== 'admin') {
-      return res.status(401).json({
-        message: 'Hi! This resource can only be accessed by an admin',
-        error: true,
-      });
-    }
-    return next();
-  });
+  const { decoded } = req;
+  if (decoded.type !== 'admin') {
+    return res.status(401).json({
+      message: 'Hi! This resource can only be accessed by an admin',
+      error: true,
+    });
+  }
+  return next();
 };
 
 const verifyAttendant = (req, res, next) => {
-  const token = req.headers.accesstoken;
-  jwt.verify(token, secret, (err, authdata) => {
-    if (authdata.type !== 'attendant') {
-      return res.status(401).json({
-        message: 'Hi! This resource can only be accessed by an attendant',
-        error: true,
-      });
-    }
-    return next();
-  });
+  const { decoded } = req;
+  if (decoded.type !== 'attendant') {
+    return res.status(401).json({
+      message: 'Hi! This resource can only be accessed by an attendant',
+      error: true,
+    });
+  }
+  return next();
 };
 
-export { authenticate, verifyAdmin, verifyAttendant };
+export {
+  authenticate,
+  verifyAdmin,
+  verifyAttendant,
+};
