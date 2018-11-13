7b1ec0aee257e1d4c1e0de552a1da52e9e1c1682
Ebenezer <ebenezerdonu@gmail.com>
2018-11-13 10:31:23 +0100

    Make addProduct return message Json

--- server/routes/controllers/productsController.js
+++ server/routes/controllers/productsController.js
@@ -53,7 +53,10 @@ const addProduct = (req, res) => {
       if (err) {
         throw err;
       }
-      return res.status(200).json(data.rows[0]);
+      return res.status(201).json({
+        message: data.rows[0],
+        success: true,
+      });
     });
   });
 };
