003a164035e03f7c40ecaa3752e8c2194d5272fb
Ebenezer <ebenezerdonu@gmail.com>
2018-11-16 19:25:52 +0100

    Get sales from api

--- UI/assets/js/admindashboard.js
+++ UI/assets/js/admindashboard.js
@@ -19,4 +19,47 @@ const getUsers = () => {
     });
 };
 
+const getSales = () => {
+  fetch(`${url}/sales`, {
+    headers: {
+      'Content-Type': 'application/json',
+      accesstoken: localStorage.accesstoken,
+    },
+  })
+    .then(res => res.json())
+    .then((data) => {
+      let output;
+      for (let i = 0; i < 4; i += 1) {
+        output = `
+        <li><a href="product-item.html">${data[i].productname}</a></li>
+              `;
+        document.getElementById('sale-list').innerHTML += output;
+      }
+    });
+};
+
+const getProducts = () => {
+  fetch(`${url}/products`, {
+    headers: {
+      'Content-Type': 'application/json',
+      accesstoken: localStorage.accesstoken,
+    },
+  })
+    .then(res => res.json())
+    .then((data) => {
+      let output;
+      for (let i = 0; i < 4; i += 1) {
+        output = `
+        <a href="product-item.html">
+              <img src='${data[i].productimage}'
+                width='300'>
+            </a>
+              `;
+        document.getElementById('t-products').innerHTML += output;
+      }
+      document.getElementById('t-products').innerHTML += '<a href="products.html" class="green-button">See All Products</a>';
+    });
+};
+
 window.onload = getUsers();
+window.onload = getSales();
