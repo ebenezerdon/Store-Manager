c970dc25eb83a06ec606d4dd0b7f1d985417c131
Ebenezer <ebenezerdonu@gmail.com>
2018-11-19 04:41:19 +0100

    Add logout button

--- UI/assets/js/main.js
+++ UI/assets/js/main.js
@@ -1,5 +1,15 @@
+const logoutButton = document.getElementById('logout-button');
+
 const checkLoginstatus = () => {
   if (!localStorage.accesstoken) {
     window.location.replace('./index.html');
   }
 };
+
+const logout = () => {
+  alert('Hey!');
+  localStorage.removeItem('accesstoken');
+};
+
+window.onload = checkLoginstatus;
+logoutButton.addEventListener('click', logout);
--- UI/assets/js/nav.js
+++ UI/assets/js/nav.js
@@ -3,10 +3,10 @@ const nav = `
     <div class="logo">
       <a href="../index.html"><img src="../assets/images/logo.png" alt=""></a>
     </div>
+    <p id="logout-button">Logout</p>
     <a href="attendantdashboard.html"><i class="far fa-user-circle"></i></a>
     <a href="admindashboard.html"><i class="far fa-user-circle"></i></a>
     <a href="#"><i class="far fa-bell"></i></a>
-    <p>Hello, <span id="name">Josh</span></p>
   </div>
   <div class="nav-2">
     <div class="wrap">
